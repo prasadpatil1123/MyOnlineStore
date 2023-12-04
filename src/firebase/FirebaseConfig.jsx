d90eece32171829bf5a466e4b4697bc4eaa9de7e
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2023-12-04 23:28:57 +0530

    firebase setting

---
+++ src/firebase/FirebaseConfig.jsx
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBaRrsOzR2ptE7uxvD74jGnWr_thVrF-yU",
+  authDomain: "myonlinestore-26dbe.firebaseapp.com",
+  projectId: "myonlinestore-26dbe",
+  storageBucket: "myonlinestore-26dbe.appspot.com",
+  messagingSenderId: "877407104990",
+  appId: "1:877407104990:web:e035d1619d3208d9f1774f"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const fireDB = getFirestore(app);
+const auth = getAuth(app);
+
+export {fireDB,auth}
